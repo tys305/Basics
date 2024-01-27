7189198f77674698f496923e037c692281560060
Tyler <tys305@vt.edu>
2024-01-26 23:29:53 -0500

    Added fizzbuzz and languages

---
+++ fizzbuzz.java
@@ -0,0 +1,19 @@
+
+public class fizzbuzz {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 20; i++) {
+			if (i % 3 != 0 && i % 5 != 0) {
+				System.out.print(i);
+			}
+			
+			if (i % 3 == 0) {
+				System.out.print("Fizz");
+			}
+			if (i % 5 == 0) {
+				System.out.print("Buzz");
+			}
+			
+			System.out.println();
+		}
+	}
+}
